ee7dbb5ca2fdfcc1cbb3ec28e4b6882903d7489e
Tergelle <b.tergel@letumongolia.com>
2025-05-11 14:29:06 +0800

    Update 3_About.py

--- resume_app/pages/3_About.py
+++ resume_app/pages/3_About.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="About", layout="centered")
 
-# --- Consistent CSS, card style ---
+# --- CSS, card style ---
 st.markdown("""
     <style>
     .stApp { background-color: #f7fafd; }
